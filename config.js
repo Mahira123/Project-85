462a338f81bb8b6500329aece7efd82405ab3cf9
Mahira123 <72437265+Mahira123@users.noreply.github.com>
2022-05-08 12:26:49 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAYBrax2JW5-A_3oU_qi_0z6bOkc6S5kfo",
+    authDomain: "spectagram-app-f880c.firebaseapp.com",
+    projectId: "spectagram-app-f880c",
+    storageBucket: "spectagram-app-f880c.appspot.com",
+    messagingSenderId: "162464999114",
+    appId: "1:162464999114:web:ea185b8dca93ede3163633"
+  };
+
+  
